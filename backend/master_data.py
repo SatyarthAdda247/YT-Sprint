898f400cd76e9191c7af83f597faf5c62f6d7611
Satyarth P. Srivastava <satyarth.s.2003@gmail.com>
2025-11-13 20:04:27 +0530

    Add 'All Subjects' as first option in all exams
    FEATURE:
    ✅ Added 'All Subjects' as first subject option
    ✅ Appears above 'None' and all other subjects
    ✅ Available in all 12 verticals
    
    SUBJECT ORDER NOW:
    1. All Subjects ← NEW!
    2. None
    3. Specific subjects...
    
    USE CASES:
    - Content covering multiple subjects
    - General exam preparation videos
    - Cross-subject tips & strategies
    - Overview/introduction content
    
    BENEFITS:
    - Clear distinction from 'None'
    - Better categorization for multi-subject content
    - Easier filtering and search

--- backend/master_data.py
+++ backend/master_data.py
@@ -3,23 +3,23 @@
 MASTER_DATA = {
     "Bank Pre": {
         "exams": ["SBI Clerk", "SBI PO", "IBPS CLERK", "IBPS PO", "LIC AAO", "RRB PO", "RRB Clerk"],
-        "subjects": ["None", "Reasoning", "Quants", "English", "General Awareness", "Current Affairs", "Hindi", "Computer"]
+        "subjects": ["All Subjects", "None", "Reasoning", "Quants", "English", "General Awareness", "Current Affairs", "Hindi", "Computer"]
     },
     "Bank Post": {
         "exams": ["JAIIB", "CAIIB", "IIBF CERTIFICATION COURSE", "BANK PROMOTION EXAMS"],
-        "subjects": ["None", "AFM", "RBWM", "IEIFS", "PPB", "ABM", "ABFM", "BFM", "BRBL", "CAIIB Elective Subjects", 
+        "subjects": ["All Subjects", "None", "AFM", "RBWM", "IEIFS", "PPB", "ABM", "ABFM", "BFM", "BRBL", "CAIIB Elective Subjects", 
                     "CCP + AML", "Foreign Exchange", "Prevention of Cyber Crime", "KYC + IBC + MSME", 
                     "General Banking", "Computer Knowledge", "Banking Law"]
     },
     "SSC": {
         "exams": ["GD", "MTS", "CHSL", "CGL", "Delhi Police", "CPO", "Steno", "RRB NTPC", "ALP", "Group D", "RPF"],
-        "subjects": ["None", "Current Affairs", "English", "GK/GS", "Maths", "Reasoning", "Science", "Shorthand"]
+        "subjects": ["All Subjects", "None", "Current Affairs", "English", "GK/GS", "Maths", "Reasoning", "Science", "Shorthand"]
     },
     "Teaching": {
         "exams": ["CTET", "LT Grade", "Bihar STET", "EMRS", "UP GIC", "NVS", "KVS", "HTET", "BPSC TRE 4.0", 
                  "UP TET", "REET", "DSSSB", "TGT", "PGT", "PRT", "TET Exams", "AWES", "SET Exams", 
                  "Super TET", "RPSC Teaching Exam", "Sainik School Exams", "West Bengal SSC Teacher Recruitment"],
-        "subjects": ["None", "English", "Hindi", "Maths", "Sanskrit", "CDP", "EVS", "General Studies", "Commerce", "Urdu", 
+        "subjects": ["All Subjects", "None", "English", "Hindi", "Maths", "Sanskrit", "CDP", "EVS", "General Studies", "Commerce", "Urdu", 
                     "Social Studies", "Science", "Home Science", "Music", "Arts", "Social Science", 
                     "Physical Education", "Fine Arts", "Physics", "Chemistry", "Biology", "Zoology", "History", 
                     "Geography", "Political Science", "Sociology", "Economics", "Philosophy", "Psychology", 
@@ -28,7 +28,7 @@ MASTER_DATA = {
     },
     "UGC": {
         "exams": ["Paper 1", "Paper 2", "SET / SLET", "CSIR NET"],
-        "subjects": ["None", "General Paper", "Political Science", "Philosophy", "Psychology", "Sociology", "History", 
+        "subjects": ["All Subjects", "None", "General Paper", "Political Science", "Philosophy", "Psychology", "Sociology", "History", 
                     "Commerce", "Education", "Home Science", "Physical Education", "Law", "Music", "Sanskrit", 
                     "Geography", "Ayurveda", "Biology", "Hindi", "Environmental Sciences", 
                     "Computer Science and Applications", "Library and Information Science", "Urdu", "English", 
@@ -38,13 +38,13 @@ MASTER_DATA = {
     "Bihar": {
         "exams": ["BPSC AEDO", "BSSC CGL-4", "Bihar Jeevika", "Bihar SI Daroga", "BSSC STENO", 
                  "BSSC Inter level", "BSSC Karyalay parichari", "Bihar Police driver"],
-        "subjects": ["None", "Hindi", "Maths", "GK/GS", "Reasoning", "English", "Science", "Current Affairs", 
+        "subjects": ["All Subjects", "None", "Hindi", "Maths", "GK/GS", "Reasoning", "English", "Science", "Current Affairs", 
                     "Subject Knowledge", "Computer", "Static GK"]
     },
     "Punjab": {
         "exams": ["PSSSB", "Punjab police constable", "High court", "ETT/NTT", "PSTET", "Master Cadre", 
                  "Punjab PCS", "SSC", "Railways"],
-        "subjects": ["None", "Static & Current Affairs", "General Knowledge", "Basic Computer Knowledge", 
+        "subjects": ["All Subjects", "None", "Static & Current Affairs", "General Knowledge", "Basic Computer Knowledge", 
                     "Logical Reasoning", "Quantitative Aptitude", "Numerical Aptitude", "General English", 
                     "Punjabi Language", "Punjab GK", "General Awareness", "Arithmetic", "Teaching Aptitude", 
                     "Pedagogy", "Information & Communication Technology (ICT)", "Hindi Language", 
@@ -55,13 +55,13 @@ MASTER_DATA = {
     "Odia": {
         "exams": ["Bed Entrance Exam", "LTR MAINS ARTS", "OSSC CGL", "OSSC PEO", "SSD Sevak Sevika", 
                  "Police Constable", "RI AMIN MAINS", "RRB NTPC", "RRB Group D", "RRb PO", "IBPS Clerk", "OPSC"],
-        "subjects": ["None", "Current Affairs", "Reasoning", "English", "GK/GS", "Geography", "History", "Polity", 
+        "subjects": ["All Subjects", "None", "Current Affairs", "Reasoning", "English", "GK/GS", "Geography", "History", "Polity", 
                     "Pedagogy", "Computer", "Physics", "Chemistry", "Mathematics", "Economics"]
     },
     "Telugu": {
         "exams": ["NTPC", "Group-D", "RRB Junior Engineer (CBT-1 Only)", "MTS", "CHSL", "GD", "CGL", 
                  "Bank PO", "Bank Clerk", "APPSC & TGPSC"],
-        "subjects": ["None", "Mathematics", "Reasoning", "Polity", "Economy", "History", "Geography", "Current Affairs", 
+        "subjects": ["All Subjects", "None", "Mathematics", "Reasoning", "Polity", "Economy", "History", "Geography", "Current Affairs", 
                     "Computer", "Arithmetic", "English", "Banking/Financial Awareness", "Credit Co-Operative", 
                     "Science & Tech", "Telangana Movement (for Telangana Exams only)", 
                     "General Science (Physics + Chemistry + Biology)", "Teaching Aptitude", "Pedagogy", 
@@ -69,12 +69,12 @@ MASTER_DATA = {
     },
     "Tamil": {
         "exams": ["TNPSC", "TET", "NTPC", "TNUSRB Si", "PC", "IB", "RPF", "RRB JE", "RRB GR D"],
-        "subjects": ["None", "Current Affairs", "English", "Maths", "Geography", "Science", "Psychology", "GK", 
+        "subjects": ["All Subjects", "None", "Current Affairs", "English", "Maths", "Geography", "Science", "Psychology", "GK", 
                     "Reasoning", "Biology", "Polity", "History", "GS"]
     },
     "Bengal": {
         "exams": ["WBSSC GROUP C & D", "SSC MTS", "RRB NTPC", "WBP", "Banking", "WBCS"],
-        "subjects": ["None", "Current Affairs", "History", "Polity", "Mathematics", "Gk", "Gs", "English", 
+        "subjects": ["All Subjects", "None", "Current Affairs", "History", "Polity", "Mathematics", "Gk", "Gs", "English", 
                     "General Studies", "Static Gk", "Reasoning", "Banking Awareness", "Geography"]
     },
     "Agriculture": {
@@ -84,7 +84,7 @@ MASTER_DATA = {
                  "BPSC BAO/SDAO", "BHO/SHDO", "Bihar Jeevika Bharti", "UPSSSC AGTA", "Cane Supervisor", 
                  "MP ESB", "RPSC Agriculture Supervisor", "DDA SO Horticulture", "DSSSB SO Horticulture", 
                  "NHB SHO", "CCI JCE", "CWC JTA", "BSSC Field Assistant"],
-        "subjects": ["None", "Agronomy", "Genetics & Plant Breeding", "Entomology", "Soil Science", 
+        "subjects": ["All Subjects", "None", "Agronomy", "Genetics & Plant Breeding", "Entomology", "Soil Science", 
                     "Agri. Current Affairs", "Horticulture", "Allied Agriculture", "Animal Husbandry", 
                     "Plant Pathology", "Food Science & Technology"]
     }
